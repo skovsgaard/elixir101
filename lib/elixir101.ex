223b8d65a4698378874c01fef91e9f04abd79b85
skovsgaard <jonas.skovsgaard.christensen@gmail.com>
2015-06-14 13:21:11 +0200

    Simplified entrypoint

--- lib/elixir101.ex
+++ lib/elixir101.ex
@@ -1,7 +1,8 @@
 defmodule Elixir101 do
-  def prep, do: Elixir101.Builder.all
   def start do
     prep
     {:ok, _} = Plug.Adapters.Cowboy.http Elixir101.Server, []
   end
+  
+  defp prep, do: Elixir101.Builder.all
 end
